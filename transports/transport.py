2f27a9a39c0a678914aba75dcfe396f3041d908f
Thomas Penteker <tek@serverop.de>
2012-12-13 00:20:42 +0100

    transports/transport.py: Vorbereitung für modularen Handler() in sparringserver

--- transports/transport.py
+++ transports/transport.py
@@ -10,6 +10,8 @@ class Transport():
     self.own_ip = own_ip
     # Template for new connections
     self.connection = None
+    # Template for new handlers
+    self.handler = None
 
   def items(self):
     return self.connections
@@ -19,12 +21,17 @@ class Transport():
     """ conntype: connection template class for data handling
     src: client tuple (ip, port)
     dst: server tuple(ip, port) """
-    if src[0] == self.own_ip:
+    # 0.0.0.0: UDP case where dst cannot - yet - be determined in full/half
+    # mode due to python restrictions. see sparringserver.py for gory details
+    if src[0] == self.own_ip or dst[0] == '0.0.0.0':
       self.connections[src] = self.connection(self, src, dst) 
       return self.connections[src]
     else:
       self.connections[dst] = self.connection(self, dst, src) 
       return self.connections[dst]
+
+  def newhandler(self, conn, sock):
+    return self.handler(conn, sock, map=None)
 
   def classify(self, connection):
     for application in self.applications:
