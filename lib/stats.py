24394dccf5f73fa7d6a3213722fbb0a56f7a6935
Thomas Penteker <tek@serverop.de>
2012-12-13 00:17:29 +0100

    lib/stats.py: unicodeError abfangen in __str__() -> p()=

--- lib/stats.py
+++ lib/stats.py
@@ -1,7 +1,6 @@
 from socket import inet_ntoa
 
 class Stats():
-  #cats = {} # 'statisch' geteiltes Objekt fuer alle Instantenzen
   def __init__(self):
     self.cats = {}
     self.cats['Server'] = {}
@@ -16,26 +15,29 @@ class Stats():
     if not server in self.servers:
       self.servers[server] = Server(server, meta)
 
-  # default output is generated in a haskellesque way
+  """ string representation of the stats object """
   def p(self, x, level):
-    ret = ''
-    if type(x) == type([]):
-      for xs in x:
-        ret += "%s\n" % self.p(xs, level+2)
-      return ret
-    if type(x) == type(()):
-      for xs in x:
-        try:
-          ret += "%s " % inet_ntoa(xs)
-        except:
-          ret += "%s " % xs
-      return "%s%s" % (" "*level, ret)
-    elif type(x) == type({}):
-      for k, v in x.items():
-        ret += "\n%s%s\n%s" % (" "*level, self.p(k, level), self.p(v, level+2))
-      return ret
-    else:
-      return "%s%s" % (" "*(level+2), x)
+    try:
+      ret = ''
+      if type(x) == type([]):
+        for xs in x:
+          ret += "%s\n" % self.p(xs, level+2)
+        return ret
+      if type(x) == type(()):
+        for xs in x:
+          try:
+            ret += "%s " % inet_ntoa(xs)
+          except:
+            ret += "%s " % xs
+        return "%s%s" % (" "*level, ret)
+      elif type(x) == type({}):
+        for k, v in x.items():
+          ret += "\n%s%s\n%s" % (" "*level, self.p(k, level), self.p(v, level+2))
+        return ret
+      else:
+        return "%s%s" % (" "*(level+2), x)
+    except UnicodeDecodeError:
+      return "unicodeDecodeError"
 
   def __str__(self):
     return self.p(self.cats, 0)
